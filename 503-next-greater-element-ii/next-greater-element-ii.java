2a78a693452bc89437ee4e2b9729e13cc78e5638
Aayush Singh <ayushsingh4696@gmail.com>
2026-05-06 20:18:02 +0530

    Time: 19 ms (12.85%) | Memory: 49 MB (6.31%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,27 +1,39 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+
         int n = nums.length;
-        Stack <Integer> st = new Stack<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = 2*n-1;i>=0;i--){
-            if(st.size()==0) arr.add(-1);
-            else if(st.size() > 0 && st.peek() > nums[i%n]){
-                arr.add(st.peek());
+
+        Stack<Integer> st = new Stack<>();
+
+        int[] ans = new int[n];
+
+        for(int i = 2*n - 1; i >= 0; i--){
+
+            if(st.size() == 0){
+                if(i < n) ans[i] = -1;
             }
-            else if(st.size() > 0 && st.peek()<=nums[i%n]){
-                while(st.size() > 0 && st.peek()<=nums[i%n]){
+
+            else if(st.size() > 0 && st.peek() > nums[i % n]){
+                if(i < n) ans[i] = st.peek();
+            }
+
+            else if(st.size() > 0 && st.peek() <= nums[i % n]){
+
+                while(st.size() > 0 && st.peek() <= nums[i % n]){
                     st.pop();
                 }
-                if(st.size()==0) arr.add(-1);
-                else arr.add(st.peek());
+
+                if(st.size() == 0){
+                    if(i < n) ans[i] = -1;
+                }
+                else{
+                    if(i < n) ans[i] = st.peek();
+                }
             }
-            st.push(nums[i%n]);
+
+            st.push(nums[i % n]);
         }
-        Collections.reverse(arr);
-        int[] ans = new int[n];
-        for(int i = 0; i < n; i++){
-        ans[i] = arr.get(i);
-        }
+
         return ans;
     }
 }
