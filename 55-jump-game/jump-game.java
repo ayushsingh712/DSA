d40c343d4b33f31593b494de0250fb0e53a892e2
Aayush Singh <ayushsingh4696@gmail.com>
2026-08-19 21:35:34 +0530

    Time: 2 ms (88.92%) | Memory: 47.9 MB (43.49%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int reach = 0;
+        for(int i = 0;i<n;i++){
+            if(i > reach) return false;
+            else reach = Math.max(reach,i+nums[i]);
+        }
+        return (reach >= n - 1);
+    }
+}
