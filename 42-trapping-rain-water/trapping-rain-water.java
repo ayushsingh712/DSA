542f792b498b82e0b77540aca0fd65085dc11232
Aayush Singh <ayushsingh4696@gmail.com>
2026-09-12 20:07:14 +0530

    Time: 1 ms (56.23%) | Memory: 48.7 MB (20.91%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,21 +1,36 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int l = 0, r = n-1;
-        int lmax = 0, rmax = 0;
-        int ans = 0;
-        while(l < r){
-            lmax = Math.max(lmax,height[l]);
-            rmax = Math.max(rmax,height[r]);
+        // int n = height.length;
+        // int l = 0, r = n-1; // using two pointers
+        // int lmax = 0, rmax = 0;
+        // int ans = 0;
+        // while(l < r){
+        //     lmax = Math.max(lmax,height[l]);
+        //     rmax = Math.max(rmax,height[r]);
 
-            if(lmax < rmax){
-                ans = ans + lmax - height[l];
-                l++;
-            }
-            else {
-                ans = ans + rmax - height[r];
-                r--;
-            }
+        //     if(lmax < rmax){
+        //         ans = ans + lmax - height[l];
+        //         l++;
+        //     }
+        //     else {
+        //         ans = ans + rmax - height[r];
+        //         r--;
+        //     }
+        // }
+        // return ans;
+        int n = height.length;
+        int [] lmax = new int[n];
+        int [] rmax = new int[n];
+        lmax[0] = height[0]; rmax[n-1] = height[n-1];
+        for(int i = 1;i<n;i++){
+            lmax[i] = Math.max(lmax[i-1],height[i]);
+        }
+        for(int i = n-2;i>=0;i--){
+            rmax[i] = Math.max(rmax[i+1],height[i]);
+        }
+        int ans = 0; 
+        for(int i = 0;i<n;i++){
+            ans = ans + Math.min(lmax[i],rmax[i]) - height[i];
         }
         return ans;
     }
